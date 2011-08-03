7c822bb2dd3d932b4edc852fbc800b6b24644368
Mikhail Troshev <mishanga@yandex-team.ru>
2011-08-04 00:55:47 +0400

    Initial commit

---
+++ pages/form/form.bemjson.js
@@ -0,0 +1,15 @@
+({
+    block: 'b-page',
+    title: 'BEM Form',
+    head: [
+        { elem: 'css', url: 'form.css'},
+        { elem: 'css', url: 'form.ie.css', ie: true },
+        { elem : 'js', url: '//yandex.st/jquery/1.6.2/jquery.min.js' },
+        { elem: 'js', url: 'form.js' }
+    ],
+    content: [
+        {
+            content : 'BEM Form'
+        }
+    ]
+})
