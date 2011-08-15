8ca70a0b90526d3e749c894b1db74663195bd671
Mikhail Troshev <mishanga@yandex-team.ru>
2011-08-15 13:35:18 +0400

    small bemjson fix

--- pages/form/form.bemjson.js
+++ pages/form/form.bemjson.js
@@ -5,7 +5,7 @@
     head: [
         { elem: 'css', url: 'form.css'},
         { elem: 'css', url: 'form.ie.css', ie: true },
-        { elem : 'js', url: '//yandex.st/jquery/1.6.2/jquery.min.js' },
+        { elem: 'js', url: '//yandex.st/jquery/1.6.2/jquery.min.js' },
         { elem: 'js', url: 'form.js' }
     ],
     content: {
