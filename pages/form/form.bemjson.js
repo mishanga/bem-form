5272f4a797a724f949133530a21854c0563a59e2
Mikhail Troshev <mishanga@yandex-team.ru>
2011-08-23 19:49:48 +0400

    Some beautifications: fonts, layout.

--- pages/form/form.bemjson.js
+++ pages/form/form.bemjson.js
@@ -11,115 +11,128 @@
     content: {
         block: 'i-jz-form',
         cls: 'jz',
-        content: [
-            {
-                tag: 'h1',
-                content: 'BEM Form'
-            },
-            {
-                block: 'b-form-widget',
+        content: {
+            block: 'b-layout-table',
+            content: {
+                elem: 'row',
                 content: [
+                    { elem: 'gap' },
                     {
-                        elem: 'label',
-                        content: {
-                            tag: 'label',
-                            content: 'Фамилия'
-                        }
-                    },
-                    {
-                        elem: 'field',
-                        content: {
-                            block: 'b-input',
-                            name: 'lastname',
-                            jz: {
-                                container: '.b-form-widget',
-                                required: {}
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                block: 'b-form-widget',
-                content: [
-                    {
-                        elem: 'label',
-                        content: {
-                            tag: 'label',
-                            content: 'Имя'
-                        }
-                    },
-                    {
-                        elem: 'field',
-                        content: {
-                            block: 'b-input',
-                            name: 'firstname',
-                            jz: {
-                                container: '.b-form-widget',
-                                required: {}
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                block: 'b-form-widget',
-                content: [
-                    {
-                        elem: 'label',
-                        content: {
-                            tag: 'label',
-                            content: 'Отчество'
-                        }
-                    },
-                    {
-                        elem: 'field',
-                        content: {
-                            block: 'b-input',
-                            name: 'middlename',
-                            jz: {
-                                container: '.b-form-widget',
-                                enabled: { 'name': 'firstname' }
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                block: 'b-form-widget',
-                content: [
-                    {
-                        elem: 'label',
-                        content: {
-                            tag: 'label',
-                            content: 'Электропочта'
-                        }
-                    },
-                    {
-                        elem: 'field',
-                        content: {
-                            block: 'b-input',
-                            name: 'email',
-                            jz: {
-                                container: '.b-form-widget',
-                                valid: { type: 'email' }
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                block: 'b-form-widget',
-                content: {
-                    elem: 'button',
-                    content: {
-                        block: 'b-button',
-                        value: 'Сохранить',
-                        jz: true
-                    }
+                        elem: 'cell',
+                        content: [
+                            {
+                                tag: 'h1',
+                                content: 'BEM Form'
+                            },
+                            {
+                                block: 'b-form-widget',
+                                content: [
+                                    {
+                                        elem: 'label',
+                                        content: {
+                                            tag: 'label',
+                                            content: 'Фамилия'
+                                        }
+                                    },
+                                    {
+                                        elem: 'field',
+                                        content: {
+                                            block: 'b-input',
+                                            name: 'lastname',
+                                            jz: {
+                                                container: '.b-form-widget',
+                                                required: {}
+                                            }
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                block: 'b-form-widget',
+                                content: [
+                                    {
+                                        elem: 'label',
+                                        content: {
+                                            tag: 'label',
+                                            content: 'Имя'
+                                        }
+                                    },
+                                    {
+                                        elem: 'field',
+                                        content: {
+                                            block: 'b-input',
+                                            name: 'firstname',
+                                            jz: {
+                                                container: '.b-form-widget',
+                                                required: {}
+                                            }
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                block: 'b-form-widget',
+                                content: [
+                                    {
+                                        elem: 'label',
+                                        content: {
+                                            tag: 'label',
+                                            content: 'Отчество'
+                                        }
+                                    },
+                                    {
+                                        elem: 'field',
+                                        content: {
+                                            block: 'b-input',
+                                            name: 'middlename',
+                                            jz: {
+                                                container: '.b-form-widget',
+                                                enabled: { 'name': 'firstname' }
+                                            }
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                block: 'b-form-widget',
+                                content: [
+                                    {
+                                        elem: 'label',
+                                        content: {
+                                            tag: 'label',
+                                            content: 'Электропочта'
+                                        }
+                                    },
+                                    {
+                                        elem: 'field',
+                                        content: {
+                                            block: 'b-input',
+                                            name: 'email',
+                                            jz: {
+                                                container: '.b-form-widget',
+                                                valid: { type: 'email' }
+                                            }
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                block: 'b-form-widget',
+                                content: {
+                                    elem: 'button',
+                                    content: {
+                                        block: 'b-button',
+                                        value: 'Сохранить',
+                                        jz: true
+                                    }
 
-                }
+                                }
+                            }
+                        ]
+                    },
+                    { elem: 'gap' }
+                ]
             }
-        ]
+        }
     }
 })
